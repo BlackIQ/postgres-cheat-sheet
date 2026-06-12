740b93a12a6a5554ce3f92831ef2d8aa2fd6baaf
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-13 02:06:47 +0330

    LIMIT: Now you know what is limit

---
+++ 17-limit.pg.sql
@@ -0,0 +1,10 @@
+-- Limit
+-- Good for pagination
+
+-- Just get 3 items:
+SELECT * FROM people LIMIT 3;
+
+-- Order by age and get 5 items:
+SELECT * FROM people ORDER BY age LIMIT 5;
+
+-- Just this. See you at 18
