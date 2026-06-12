27a4ee0c426b873485d2ad8b65305fe313c8a48c
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-13 01:11:19 +0330

    Add DISTINCT

---
+++ 12-distinct.pg.sql
@@ -0,0 +1,10 @@
+-- Tell me unique jobs here
+-- Can you? NO!?
+-- So, use DISTINCT
+
+SELECT DiSTINCT job FROM people;
+
+-- Consider we have 2 programmers, 5 network admins and 10 sales
+-- What will DISTINCT returns?
+-- Programmer, Network Admins, Sales
+-- See you at 13
