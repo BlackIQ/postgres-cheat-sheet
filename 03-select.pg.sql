1b0fc0bbcc86ad3e348f6f19ac4f279fe585e776
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-13 00:36:38 +0330

    Starting SELECT

---
+++ 03-select.pg.sql
@@ -0,0 +1,16 @@
+-- SELECT: Well, command you get data with
+-- You may be familier with find()
+-- if have experiences using MongoDB
+-- Select is also like that. Get data
+
+-- At 01 and 02 we created a table
+-- Now let's get everythig from it out
+
+SELECT * FROM people;
+
+-- What is happening here?
+-- We said SELECT *
+-- It means all columns.
+-- From which table? people
+
+-- Sure we will cover SELECT more. See you in 04
