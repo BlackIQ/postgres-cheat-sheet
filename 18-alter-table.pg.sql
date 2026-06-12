c31dd53184857a93c1e4e770fb79b71d655492b7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-13 02:13:46 +0330

    ALTER TABLE: Add or Remove a column

---
+++ 18-alter-table.pg.sql
@@ -0,0 +1,11 @@
+-- How to change structure of your table?
+-- Using ALTER TABLE
+
+-- Add a column
+ALTER TABLE people ADD COLUMN is_bitch BOOL;
+
+-- Remove a column
+ALTER TABLE people DROP COLUMN employed;
+
+-- Now you fucking know how to add a column and remoe a column
+-- See you at 19
